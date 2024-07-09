5f91b8714ec42640a9dddda5353a9cda4a88576f
Akash Suryawanshi <akashsuryawanshi830@outlook.com>
2024-07-09 17:37:57 +0530

    ConfigServer service Added
    In this service we store common configuration on Git.  other services fetch config file from git

---
+++ ConfigServer/src/test/java/com/skytechconfigserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skytechconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
